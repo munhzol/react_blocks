6248aca783752c64c09d5c8235662d69fc686134
munhzol <munhzol@gmail.com>
2020-05-06 22:45:21 -0400

    docs Commit

---
+++ docs/precache-manifest.8cc5101a98076f8a82a82df0839974c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dceb17dadfc0482e4bf1cdbf578942c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f55d48dcc777329712d",
+    "url": "/static/css/main.3eb5c6f2.chunk.css"
+  },
+  {
+    "revision": "97e54766ea68a6d06b22",
+    "url": "/static/js/2.47158878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47158878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f55d48dcc777329712d",
+    "url": "/static/js/main.708f65e4.chunk.js"
+  },
+  {
+    "revision": "32736c806c587fc45db9",
+    "url": "/static/js/runtime-main.bb49bc86.js"
+  }
+]);
